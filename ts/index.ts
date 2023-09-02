91da785a1553e23c3152c483594834cfb4bb34f9
raylu <90059+raylu@users.noreply.github.com>
2023-09-01 22:34:49 -0700

    c3 chart

--- ts/index.ts
+++ ts/index.ts
@@ -1,4 +1,11 @@
-function add(a: number, b: number): number {
-	return a + b;
-}
-console.log('hello', add(1, 2));
+import * as c3 from 'c3';
+
+c3.generate({
+    'bindto': '#chart',
+    'data': {
+      'columns': [
+        ['data1', 30, 200, 100, 400, 150, -25],
+        ['data2', 50, 20, 10, 40, 15, 25],
+      ],
+    },
+});
