6c90019a82e73edef1761505948bd359df7b1f3e
raylu <90059+raylu@users.noreply.github.com>
2023-09-01 23:53:49 -0700

    fetch interval data

--- ts/index.ts
+++ ts/index.ts
@@ -1,11 +1,19 @@
 import * as c3 from 'c3';
+import {unpack} from 'msgpackr/unpack';
+
+async function fetchIntervalData(month: string): Promise<number[]> {
+	const url = `interval_data/${month}.msgpack`;
+	const resp = await fetch(url);
+	if (!resp.ok)
+		throw new Error('failed to fetch ' + url);
+	return unpack(new Uint8Array(await resp.arrayBuffer()));
+}
 
 c3.generate({
-    'bindto': '#chart',
-    'data': {
-      'columns': [
-        ['data1', 30, 200, 100, 400, 150, -25],
-        ['data2', 50, 20, 10, 40, 15, 25],
-      ],
-    },
+	'bindto': '#chart',
+	'data': {
+		'columns': [
+			['data1', ...await fetchIntervalData('2021-01')],
+		],
+	},
 });
