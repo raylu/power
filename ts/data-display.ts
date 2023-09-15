dc03e93ac1d85f936ac80dc2cefc69f63be18de5
vinceye <vinceye.cm@gmail.com>
2023-09-15 12:19:39 -0700

    added getMonthRange logic to retrieve data from multiple months

--- ts/data-display.ts
+++ ts/data-display.ts
@@ -7,16 +7,45 @@ interface HourData {
 	$: number,
 }
 
+function getMonthRange(startDate: string, endDate?: string) {
+	const months = [];
+	const date1 = new Date(startDate);
+	const date2 = new Date(endDate);
+	const monthDiff = date2.getMonth() - date1.getMonth() + (date2.getFullYear() - date1.getFullYear()) * 12;
+	months.push(date1.toLocaleDateString('sv').slice(0,7));
+
+	if (monthDiff > 0) {
+		for (let i = 0; i < monthDiff; i++) {
+			date1.setMonth(date1.getMonth() + 1, 1);
+			months.push(date1.toLocaleDateString('sv').slice(0,7));
+		}
+	}
+
+	return months;
+}
+
 async function fetchData(month: string): Promise<HourData[]> {
 	const url = `interval_data/${month}.msgpack`;
-	const resp = await fetch(url);
-	if (!resp.ok)
-		throw new Error('failed to fetch ' + url);
-	return unpack(new Uint8Array(await resp.arrayBuffer()));
+	try {
+		const resp = await fetch(url);
+		if (!resp.ok)
+			throw new Error('failed to fetch ' + url);
+		return unpack(new Uint8Array(await resp.arrayBuffer()));
+	} catch(error) {
+		console.log(error);
+	}
 }
 
 async function generateChart(startDate?: string, endDate?: string) {
-	const intervalData = await fetchData('2023-08');
+	let intervalData = [];
+	const monthRange = getMonthRange(startDate, endDate);
+	for (const month in monthRange) {
+		const monthData = await fetchData(monthRange[month]);
+		if (monthData) {
+			intervalData = intervalData.concat(monthData);	
+		}
+	}
+
 	const testData = [];
 
 	if (startDate && endDate) {
