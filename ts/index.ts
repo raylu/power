e94e0c9efc0148e4f7aba4be1ebc7d04b58d7ada
raylu <90059+raylu@users.noreply.github.com>
2023-10-08 23:39:02 -0700

    get rid of page-load.ts

--- ts/index.ts
+++ ts/index.ts
@@ -1,4 +1,22 @@
-import {displayChart, displayHeader} from './page-load';
+import generateChart from './data-display';
 
-displayHeader();
-displayChart();
+const today = new Date();
+const formattedEndDate = today.toLocaleDateString('sv');
+const startDate = new Date(new Date().setDate(new Date().getDate()-30));
+const formattedStartDate = startDate.toLocaleDateString('sv');
+
+const handleDateFilterSubmit = (formID: string) => {
+	const startDateValue = (<HTMLInputElement>document.getElementById(`${formID}-start`)).value;
+	const endDateValue = (<HTMLInputElement>document.getElementById(`${formID}-end`)).value;
+
+	generateChart(startDateValue, endDateValue);
+};
+
+const chartFilter = document.querySelector('#power-chart-form');
+(chartFilter.querySelector('input[name="power-chart-start"]') as HTMLInputElement).value = formattedStartDate;
+(chartFilter.querySelector('input[name="power-chart-end"]') as HTMLInputElement).value = formattedEndDate;
+chartFilter.addEventListener('submit', (event) => {
+	event.preventDefault();
+	handleDateFilterSubmit(chartFilter.id);
+});
+generateChart(formattedStartDate, formattedEndDate);
