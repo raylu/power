c7166f45bb89efe9f1865680e8b3be441e5f2816
raylu <90059+raylu@users.noreply.github.com>
2023-10-16 21:12:27 -0700

    write baseload, charging, all data to single file

--- get_data.py
+++ get_data.py
@@ -61,13 +61,35 @@ async def download_month(client: opower.Opower, account: opower.Account, start: 
 	out_path.write_bytes(ormsgpack.packb(data))
 
 def analyze():
-	intervals = sorted(iter_intervals(), key=lambda interval: interval['kWh'])
-	p5 = len(intervals) // 20
-	print(len(intervals), 'data points; 5th percentile is', p5)
-	print(intervals[p5 + 1], 'kWh')
+	intervals = list(iter_intervals())
+
+	sorted_intervals = sorted(intervals, key=lambda interval: interval['kWh'])
+	p5_index = len(sorted_intervals) // 20
+	print(len(sorted_intervals), 'data points; 5th percentile is', p5_index)
+	p5 = sorted_intervals[p5_index + 1]
+	print(f"{p5['start']}\t{p5['kWh']} kWh\n")
+
+	for i, interval in enumerate(intervals):
+		date, time = interval['start'].split('T', 1)
+		hour = time.split(':', 1)[0]
+		if hour == '01' and interval['kWh'] > 4.5:
+			if intervals[i-1]['kWh'] > interval['kWh'] * 2/3:
+				print(f'charging on {date} including at 00:15')
+				intervals[i-1]['charging'] = True
+			else:
+				print(f'charging on {date} but not at 00:15')
+		if hour < '12' and interval['kWh'] > 4.5:
+			interval['charging'] = True
+
+	out_path = (CURRENT_DIR / 'interval_data.msgpack')
+	print('\nwriting', out_path)
+	out_path.write_bytes(ormsgpack.packb({
+		'baseload': p5['kWh'],
+		'intervalData': intervals,
+	}))
 
 def iter_intervals() -> typing.Iterator[dict]:
-	for path in DATA_DIR.iterdir():
+	for path in sorted(DATA_DIR.iterdir()):
 		for interval in ormsgpack.unpackb(path.read_bytes()):
 			yield interval
 
