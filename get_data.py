3f0604f93f43a8e2cf20e9a5fae03f4088201217
raylu <90059+raylu@users.noreply.github.com>
2024-02-03 19:50:03 -0800

    add quiet mode

--- get_data.py
+++ get_data.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import argparse
 import asyncio
 import calendar
 import datetime
@@ -14,11 +15,26 @@ import ormsgpack
 CURRENT_DIR = pathlib.Path(__file__).parent
 DATA_DIR = CURRENT_DIR / 'interval_data'
 
+quiet = False
+
 def main() -> None:
-	if len(sys.argv) == 2 and sys.argv[1] == 'analyze':
+	global quiet
+	parser = argparse.ArgumentParser()
+	parser.add_argument('mode')
+	parser.add_argument('-q', '--quiet', action='store_true')
+	args = parser.parse_args()
+	quiet = args.quiet
+
+	if args.mode == 'analyze':
 		analyze()
-	else:
+	elif args.mode == 'download':
 		asyncio.run(download())
+	else:
+		sys.exit('mode must be one of "analyze" or "download"')
+
+def log(*args) -> None:
+	if not quiet:
+		print(*args)
 
 async def download() -> None:
 	DATA_DIR.mkdir(exist_ok=True)
@@ -43,7 +59,7 @@ async def download() -> None:
 
 		tasks = []
 		while start < today:
-			print('will download', start.strftime('%Y-%m'))
+			log('will download', start.strftime('%Y-%m'))
 			tasks.append(download_month(client, account, start))
 			start = (start + datetime.timedelta(days=31)).replace(day=1)
 		await asyncio.gather(*tasks)
@@ -57,7 +73,7 @@ async def download_month(client: opower.Opower, account: opower.Account, start: 
 	data = [{'start': r.start_time, 'kWh': r.consumption, '$': r.provided_cost} for r in reads]
 
 	out_path = (DATA_DIR / f'{start.strftime("%Y-%m")}.msgpack')
-	print('writing', out_path)
+	log('writing', out_path)
 	out_path.write_bytes(ormsgpack.packb(data))
 
 def analyze():
@@ -65,24 +81,24 @@ def analyze():
 
 	sorted_intervals = sorted(intervals, key=lambda interval: interval['kWh'])
 	p5_index = len(sorted_intervals) // 20
-	print(len(sorted_intervals), 'data points; 5th percentile is', p5_index)
+	log(len(sorted_intervals), 'data points; 5th percentile is', p5_index)
 	p5 = sorted_intervals[p5_index + 1]
-	print(f"{p5['start']}\t{p5['kWh']} kWh\n")
+	log(f"{p5['start']}\t{p5['kWh']} kWh\n")
 
 	for i, interval in enumerate(intervals):
 		date, time = interval['start'].split('T', 1)
 		hour = time.split(':', 1)[0]
 		if hour == '01' and interval['kWh'] > 4.5:
 			if intervals[i-1]['kWh'] > interval['kWh'] * 2/3:
-				print(f'charging on {date} including at 00:15')
+				log(f'charging on {date} including at 00:15')
 				intervals[i-1]['charging'] = True
 			else:
-				print(f'charging on {date} but not at 00:15')
+				log(f'charging on {date} but not at 00:15')
 		if hour < '12' and interval['kWh'] > 4.5:
 			interval['charging'] = True
 
 	out_path = (CURRENT_DIR / 'interval_data.msgpack')
-	print('\nwriting', out_path)
+	log('\nwriting', out_path)
 	out_path.write_bytes(ormsgpack.packb({
 		'baseload': p5['kWh'],
 		'intervalData': intervals,
